641ee3f1dbb3fde4c5ae71077f0159f88a9a2714
NetAcad DEVASC <changeme@example.com>
2023-07-08 23:09:58 +0000

    Agregado script de integrantes Pozo - Neira

---
+++ integrantes.py
@@ -0,0 +1,8 @@
+integrantes = ["Paola Pozo", "Juan Neira"]
+
+def imprimir_integrantes():
+    for integrante in integrantes:
+        print(integrante)
+
+if __name__ == "__main__":
+    imprimir_integrantes()
