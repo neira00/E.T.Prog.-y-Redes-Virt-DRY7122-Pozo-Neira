dd99a95be004be32d37467ec57e79c9e27fca25f
NetAcad DEVASC <changeme@example.com>
2023-07-10 02:49:18 +0000

    Agregando script para obtener info de Router CSR1000v utilizando netmiko en Python

---
+++ info_rCSR1Kv.py
@@ -0,0 +1,18 @@
+import requests
+import netmiko
+import json
+from netmiko import ConnectHandler
+cisco1 = { "ip": "192.168.56.104",
+"device_type": "cisco_ios",
+"username": "cisco",
+"password": "cisco123!",
+}
+# Show command that we execute.
+command = "show run"
+with ConnectHandler(**cisco1) as net_connect:
+    output = net_connect.send_command(command)
+# Automatically cleans-up the output so that only the show output is returned
+print()
+print(output)
+print()
+
